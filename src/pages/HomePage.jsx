ad0a711b10853cd2ccf367ca0b2f5765e56906d4
unknown <jonatanpacora@gmail.com>
2024-04-23 17:02:58 -0500

    clonando a la chamba

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Nav from '../components/Nav'
 import FondoAnimado from '../components/fondoAnimado'
 function HomePage() {
--- src/pages/InicioPage.jsx
+++ src/pages/InicioPage.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function InicioPage() {
   return (
     <div>InicioPage</div>
