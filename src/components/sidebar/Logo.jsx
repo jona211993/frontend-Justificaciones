42b585c455b7cd9fcb3693b1f7a1ed55bfa4d3b6
JONATAN PACORA VEGA <jona.jyn.27@hotmail.com>
2024-06-09 21:04:53 -0500

    agregado config de url al backend 6

--- src/components/sidebar/Logo.jsx
+++ src/components/sidebar/Logo.jsx
@@ -3,7 +3,7 @@ export const Logo = () => {
   return (
     <div className='flex items-center justify-center p-3 mt-6'>
         <div className="w-10 h-10 flex items-center justify-center">
-          <img src="src\images\icono-logo.png" alt="logo" />          
+          <img src="\images\icono-logo.png" alt="logo" />          
         </div>
     </div>
   )
--- src/pages/views/Home.jsx
+++ src/pages/views/Home.jsx
@@ -3,8 +3,8 @@ import 'animate.css';
 export const Home = () => {
   return (
     <div className=' h-full relative animate__animated animate__fadeIn'>
-        <img src="src/images/ASESORES.png" alt="fondoHome" className="absolute inset-0 w-full h-full object-cover" />
-        <img src="src/images/LOGO BLANCO.png" alt="logo" className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+        <img src="/images/ASESORES.png" alt="fondoHome" className="absolute inset-0 w-full h-full object-cover" />
+        <img src="/images/LOGO BLANCO.png" alt="logo" className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
     </div>
   );
 };
